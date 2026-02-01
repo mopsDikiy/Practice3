7bfb3c57a0ad67f46d4e06cfda588e9d46e92a5c
mopsDikiy <timurcehov41@gmail.com>
2026-02-01 14:23:17 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Practice3
+{ 
+    class Program
+    { 
+        static void Main()
+        {
+            Console.WriteLine("Введите n");
+            uint n, i = 1;
+            n = Convert.ToUInt32(Console.ReadLine());
+            double p = 0;
+            for (; i <= n; i++)
+            {
+                p += Math.Pow(i, 7);
+            }
+            Console.WriteLine("Для n = {0}", n);
+            Console.WriteLine("Результат = {0:f0}", p);
+        }
+    }
+}
